dabfd0dcf046a9eca408c7026c1988715d865273
Raqueeb Hassan <wideangle@gmail.com>
2017-09-07 11:23:59 +0600

    Update Rscript5_video10.R

--- R/Rscript5_video10.R
+++ R/Rscript5_video10.R
@@ -15,18 +15,28 @@ setwd ("~/datasets/titanic")
 
 train <- read.csv ("~/datasets/titanic/train.csv")
 
-# Look at age patterns
+# Let's look at age patterns
 
 summary(train$Age)
+
+# We need to create another feature
 
 train$Child <- 0
 
 train$Child[train$Age < 18] <- 1
 
+# Comparing more than two subsets, we need to use command "aggregate"
+
 aggregate(Survived ~ Child + Sex, data=train, FUN=sum)
+
+# Let's look at actually how many were they in each subset
 
 aggregate(Survived ~ Child + Sex, data=train, FUN=length)
 
+# comparing subsets in terms of FUN=function(x) {sum(x)/length(x)}
+
 aggregate(Survived ~ Child + Sex, data=train, FUN=function(x) {sum(x)/length(x)}) 
+
+# In percentage
 
 aggregate(Survived ~ Child + Sex, data=train, FUN=function(x) {sum(x)/length(x) * 100}
