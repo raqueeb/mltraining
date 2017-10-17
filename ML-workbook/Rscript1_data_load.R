c10a1b53ccafdc67aea5c65677861d280eade3a0
Rakibul Hassan <raqueeb@gmail.com>
2017-10-17 15:57:56 +0600

    Create Rscript1_data_load.R

---
+++ ML-workbook/Rscript1_data_load.R
@@ -0,0 +1,23 @@
+#####################################
+#      Intro to ML Traning - R      #
+# Please come back for updated file #
+#####################################
+
+# Script 1 - Video 5
+
+# Set the working directory
+
+setwd("~/datasets/titanic")
+
+# Import the training set: train
+# Your working directory might vary
+
+train <- read.csv("~/datasets/titanic/train.csv")
+
+# viewing the "train" dataframe in raw format
+
+train
+
+# viewing the "train" dataframe in table format
+
+str(train)
