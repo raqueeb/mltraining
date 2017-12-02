7009f7833d27f9f5aae824d1c7744fa91eb380ad
Rakibul Hassan <raqueeb@gmail.com>
2017-12-02 21:27:25 +0600

    Create 5th-prediction.R

---
+++ ML-workbook/5th-prediction.R
@@ -0,0 +1,76 @@
+# Set working directory and import datafiles
+# Your working directory might vary
+
+setwd("~/datasets/titanic")
+train <- read.csv("~/datasets/titanic/train.csv")
+test <- read.csv("~/datasets/titanic/test.csv")
+
+# Install and load required packages for fancy decision tree plotting
+
+library(rpart)
+library(rattle)
+library(rpart.plot)
+library(RColorBrewer)
+
+# Can we see a name?
+train$Name[1]
+
+# We need to add test and train sets for feature engineering
+
+test$Survived <- NA
+combined_set <- rbind(train, test)
+
+# Convert to a string
+combined_set$Name <- as.character(combined_set$Name)
+
+# What's in a name, again?
+combined_set$Name[1]
+
+# Find the indexes for the tile piece of the name
+strsplit(combined_set$Name[1], split='[,.]')
+strsplit(combined_set$Name[1], split='[,.]')[[1]]
+strsplit(combined_set$Name[1], split='[,.]')[[1]][2]
+
+# Engineered variable: Title
+combined_set$Title <- strsplit(combined_set$Name, split='[,.]')[[1]][2]  # Won't work!
+combined_set$Title <- sapply(combined_set$Name, FUN=function(x) {strsplit(x, split='[,.]')[[1]][2]})
+combined_set$Title <- sub(' ', '', combined_set$Title)
+# Inspect new feature
+table(combined_setned_set$Title)
+# Combined_setne small title groups
+combined_set$Title[combined_set$Title %in% c('Mme', 'Mlle')] <- 'Mlle'
+combined_set$Title[combined_set$Title %in% c('Capt', 'Don', 'Major', 'Sir')] <- 'Sir'
+combined_set$Title[combined_set$Title %in% c('Dona', 'Lady', 'the Countess', 'Jonkheer')] <- 'Lady'
+# Convert to a factor
+combined_set$Title <- factor(combined_set$Title)
+
+# Engineered variable: Family size
+combined_set$FamilySize <- combined_set$SibSp + combined_set$Parch + 1
+
+# Engineered variable: Family
+combined_set$Surname <- sapply(combined_set$Name, FUN=function(x) {strsplit(x, split='[,.]')[[1]][1]})
+combined_set$FamilyID <- paste(as.character(combined_set$FamilySize), combined_set$Surname, sep="")
+combined_set$FamilyID[combined_set$FamilySize <= 2] <- 'Small'
+# Inspect new feature
+table(combined_set$FamilyID)
+# Delete erroneous family IDs
+famIDs <- data.frame(table(combined_set$FamilyID))
+famIDs <- famIDs[famIDs$Freq <= 2,]
+combined_set$FamilyID[combined_set$FamilyID %in% famIDs$Var1] <- 'Small'
+# Convert to a factor
+combined_set$FamilyID <- factor(combined_set$FamilyID)
+
+# Split back into test and train sets
+train <- combined_set[1:891,]
+test <- combined_set[892:1309,]
+
+# Build a new tree with our new features
+fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked + Title + FamilySize + FamilyID,
+             data=train, method="class")
+             
+fancyRpartPlot(fit)
+
+# Now let's make a prediction and write a submission file
+prediction_5th <- predict(fit, test, type = "class")
+submit <- data.frame(PassengerId = test$PassengerId, Survived = prediction_5th)
+write.csv(submit, file = "prediction5th.csv", row.names = FALSE)
