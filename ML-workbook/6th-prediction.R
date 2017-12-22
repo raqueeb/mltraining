c1c134f903bf6d0289c3ad82946a753fd7c26f69
Rakibul Hassan <raqueeb@gmail.com>
2017-12-22 19:27:16 +0600

    Update 6th-prediction.R

--- ML-workbook/6th-prediction.R
+++ ML-workbook/6th-prediction.R
@@ -8,10 +8,6 @@ test <- read.csv("~/datasets/titanic/test.csv")
 # We need to Install + load packages for decision trees and random forests
 
 library(rpart)
-install.packages('randomForest')
-library(randomForest)
-install.packages('party')
-library(party)
 
 # Join together the test and train sets for easier feature engineering
 test$Survived <- NA
@@ -36,18 +32,29 @@ combined_set$FamilySize <- combined_set$SibSp + combined_set$Parch + 1
 # Engineered variable: Family
 combined_set$Surname <- sapply(combined_set$Name, FUN=function(x) {strsplit(x, split='[,.]')[[1]][1]})
 combined_set$FamilyID <- paste(as.character(combined_set$FamilySize), combined_set$Surname, sep="")
-combined_set$FamilyID[combined_set$FamilySize <= 2] <- 'Small'
-# Delete erroneous family IDs
+
+combined_set$FamilyID[combi$FamilySize <= 2] <- 'Small'
+# Inspect new feature
+table(combi$FamilyID)
+# Removing all erroneous family IDs
 famIDs <- data.frame(table(combined_set$FamilyID))
 famIDs <- famIDs[famIDs$Freq <= 2,]
 combined_set$FamilyID[combined_set$FamilyID %in% famIDs$Var1] <- 'Small'
 # Convert to a factor
+combi$FamilyID <- factor(combi$FamilyID)
+
+combined_set$FamilySizeGroup[combined_set$FamilySize == 1] <- 'single'
+combined_set$FamilySizeGroup[combined_set$FamilySize < 5 & combined_set$FamilySize > 1] <- 'Smaller'
+combined_set$FamilySizeGroup[combined_set$FamilySize > 4] <- 'large'
+
+# Convert to a factor
 combined_set$FamilyID <- factor(combined_set$FamilyID)
+combined_set$FamilySizeGroup <- factor(combined_set$FamilySizeGroup)
 
 # Fill in Age NAs
 summary(combined_set$Age)
 
-FillAge <- rpart(Age ~ Pclass + Mother + FamilySize + Sex + SibSp + Parch + Deck + Fare + Embarked + Title + FamilyID + FamilySize, 
+FillAge <- rpart(Age ~ Pclass + Mother + FamilySize + Sex + SibSp + Parch + Deck + Fare + Embarked + Title + FamilyID + FamilySizeGroup + FamilySize, 
                 data=combined_set[!is.na(combined_set$Age),], method="anova")
 combined_set$Age[is.na(combined_set$Age)] <- predict(FillAge, combined_set[is.na(combined_set$Age),])
 summary(combined_set)
@@ -64,7 +71,7 @@ summary(combined_set$Fare)
 which(is.na(combined_set$Fare))
 combined_set$Fare[1044] <- median(combined_set$Fare, na.rm=TRUE)
 
-# New factor for Random Forests, only allowed <32 levels, so reduce number
+# New factor for new technique , only allowed <32 levels, so reduce number
 combined_set$FamilyID2 <- combined_set$FamilyID
 # Convert back to string
 combined_set$FamilyID2 <- as.character(combined_set$FamilyID2)
@@ -76,22 +83,11 @@ combined_set$FamilyID2 <- factor(combined_set$FamilyID2)
 train <- combined_set[1:891,]
 test <- combined_set[892:1309,]
 
-# Build Random Forest Ensemble
-set.seed(415)
-fit <- randomForest(as.factor(Survived) ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked + Title + FamilySize + FamilyID2,
-                    data=train, importance=TRUE, ntree=2000)
-# Look at variable importance
-varImpPlot(fit)
-# Now let's make a prediction and write a submission file
-Prediction <- predict(fit, test)
-submit <- data.frame(PassengerId = test$PassengerId, Survived = Prediction)
-write.csv(submit, file = "firstforest.csv", row.names = FALSE)
+# Build a new tree with our new features
+dtree <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked + Title + FamilySize + FamilySizeGroup + FamilyID + FamilyID2,
+             data=train, method="class")
 
-# Build condition inference tree Random Forest
-set.seed(415)
-fit <- cforest(as.factor(Survived) ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked + Title + FamilySize + FamilyID,
-               data = train, controls=cforest_unbiased(ntree=2000, mtry=3)) 
 # Now let's make a prediction and write a submission file
-Prediction <- predict(fit, test, OOB=TRUE, type = "response")
-submit <- data.frame(PassengerId = test$PassengerId, Survived = Prediction)
-write.csv(submit, file = "ciforest.csv", row.names = FALSE)
+Prediction6th <- predict(dtree, test, type = "class")
+submit <- data.frame(PassengerId = test$PassengerId, Survived = Prediction6th)
+write.csv(submit, file = "6thprediction.csv", row.names = FALSE)
