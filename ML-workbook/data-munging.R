e688d52c4e1432df8329ac4b5d9dbdf7874a039d
Rakibul Hassan <raqueeb@gmail.com>
2018-02-05 18:18:55 +0600

    Update data-munging.R

--- ML-workbook/data-munging.R
+++ ML-workbook/data-munging.R
@@ -5,28 +5,41 @@ setwd("~/datasets/titanic")
 train <- read.csv("~/datasets/titanic/train.csv")
 
 # install.packages("Amelia")
-# install.packages("Amelia")
+install.packages("Amelia")
 
+# Need to see the missing data
 library(Amelia)
 missmap(train, main="Titanic Training Data - Missings Map", 
         col=c("yellow", "black"), legend=FALSE)
 
 # barplot for all the variables 
 
+# How many survived?
 barplot(table(train$Survived),
         names.arg = c("Perished", "Survived"),
         main="Survived (passenger fate)", col="black")
+
+# Passengers travelling in different classes
 barplot(table(train$Pclass), 
         names.arg = c("first", "second", "third"),
         main="Pclass (passenger traveling class)", col="firebrick")
+
+# How many survived gender-wise?
 barplot(table(train$Sex), main="Sex (gender)", col="darkviolet")
+
+# Age distribution in the Titanic
 hist(train$Age, main="Age", xlab = NULL, col="brown")
-barplot(table(train$SibSp), main="SibSp (siblings + spouse aboard)", 
-        col="darkblue")
-barplot(table(train$Parch), main="Parch (parents + kids aboard)", 
-        col="gray50")
-hist(train$Fare, main="Fare (fee paid for ticket[s])", xlab = NULL, 
-     col="darkgreen")
+
+# How in group people (sibling+spouse) were traveling?
+barplot(table(train$SibSp), main="SibSp (siblings + spouse aboard)", col="darkblue")
+
+# How parents and children were traveling? 
+barplot(table(train$Parch), main="Parch (parents + kids aboard)", col="gray50")
+
+# What was the fair most people paid for Titanic?
+hist(train$Fare, main="Fare (fee paid for ticket[s])", xlab = NULL, col="darkgreen")
+
+# Where most people Embarked?
 barplot(table(train$Embarked), 
         names.arg = c("Cherbourg", "Queenstown", "Southampton"),
         main="Embarked (port of embarkation)", col="sienna")
