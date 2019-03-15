9671618ffba8cf04bef8b735791483acb6d96c6d
Rakibul Hassan <raqueeb@gmail.com>
2019-03-15 11:00:14 +0600

    Update decision-tree.R

--- ML-workbook/decision-tree.R
+++ ML-workbook/decision-tree.R
@@ -15,6 +15,16 @@ library(RColorBrewer)
 
 mytree1 <- rpart(Survived ~ Sex, data=train, method="class")
 fancyRpartPlot(mytree1)
+
+round(prop.table(table(train2$Survived)),2)
+## 
+##    0    1 
+## 0.62 0.38
+round(prop.table(table(train2$Sex, train2$Survived),margin = 1),2)
+##         
+##             0    1
+##   female 0.26 0.74
+##   male   0.81 0.19
+
 mytree2 <- rpart(Survived ~ Pclass + Age, data=train, method="class")
 fancyRpartPlot(mytree2)
-
