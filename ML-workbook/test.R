88a1fefd0d4b42d4df9ae306802adce1f2675544
Rakibul Hassan <raqueeb@gmail.com>
2019-10-06 15:05:42 +0600

    Create test.R

---
+++ ML-workbook/test.R
@@ -0,0 +1,20 @@
+# Set working directory and import datafiles
+
+setwd("~/datasets/titanic")
+train <- read.csv("~/datasets/titanic/train.csv")
+test <- read.csv("~/datasets/titanic/test.csv")
+
+# library load, "rpart" is the decision tree 
+# (rpart: Recursive Partitioning and Regression Trees)
+
+library(rpart)
+
+# 
+
+model <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + 
+                 Fare + Embarked, data=train, method="class")
+
+# Now let's make a prediction and write a submission file
+my_predict <- predict(model, test, type = "class")
+test_df <- data.frame(PassengerId = test$PassengerId,
+                  Name= test$Name, Survived = my_predict) 
