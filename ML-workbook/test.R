2579b015c86420b3f16abab4500681c43b814e3d
Rakibul Hassan <raqueeb@gmail.com>
2020-01-26 12:58:52 +0600

    Update test.R

--- ML-workbook/test.R
+++ ML-workbook/test.R
@@ -1,23 +1,23 @@
-# Set working directory and import datafiles
-
-setwd("~/datasets/titanic")
+# setwd("~/datasets/titanic")
 
 train <- read.csv('https://raw.githubusercontent.com/raqueeb/mltraining/master/ML-workbook/train.csv')
 # train <- read.csv("~/datasets/titanic/train.csv")
 # test <- read.csv("~/datasets/titanic/test.csv")
 test <- read.csv('https://raw.githubusercontent.com/raqueeb/mltraining/master/ML-workbook/test.csv')
 
-# library load, "rpart" is the decision tree 
+# লাইব্রেরি লোড করি - "rpart" হচ্ছে ডিসিশন ট্রি অ্যালগরিদম 
 # (rpart: Recursive Partitioning and Regression Trees)
 
 library(rpart)
 
-# 
+# একটা মডেল তৈরি করি 
 
 model <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + 
                  Fare + Embarked, data=train, method="class")
 
-# Now let's make a prediction and write a submission file
+# একটা প্রেডিকশন ডাটাফ্রেম তৈরি করি 
 my_predict <- predict(model, test, type = "class")
+
+# সেটাকে একটা ফাইলে লেখি 
 test_df <- data.frame(PassengerId = test$PassengerId,
                   Name= test$Name, Survived = my_predict) 
